4e1f8927c2ca0436358a6763b65358d62727685b
clapsheep <sktndid1203@naver.com>
2024-11-26 12:07:11 +0900

    [feat] 화면 디자인 수정

---
+++ src/components/molecules/FollowButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import BasicButton from '../atoms/BasicButton.vue'
+
+defineProps<{
+  isFollowing: boolean
+}>()
+
+defineEmits<{
+  (e: 'toggleFollow'): void
+}>()
+</script>
+
+<template>
+  <BasicButton
+    :variant="isFollowing ? 'filled' : 'outlined'"
+    :color="isFollowing ? 'warning' : 'primary'"
+    size="sm"
+    @click="$emit('toggleFollow')"
+    class="min-w-[80px] transition-all duration-200"
+  >
+    {{ isFollowing ? '팔로잉' : '팔로우' }}
+  </BasicButton>
+</template>
