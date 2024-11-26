d4e2a847b985a9a9e2c84d6faeb2790f542ceaba
clapsheep <sktndid1203@naver.com>
2024-11-26 17:54:35 +0900

    [fix] 타입에러 수정 중

--- src/core/comment/composables/useCommentManage.ts
+++ src/core/comment/composables/useCommentManage.ts
@@ -1,77 +1,92 @@
-import ApiResponse from "@/core/common/types/ApiResponse";
-import { AxiosResponse } from "axios";
-import { useGetComment } from "./useGetComment";
-import { useAddComment } from "./useAddComment";
-import { useDeleteComment } from "./useDeleteComment";
-import { useUpdateComment } from "./useUpdateComment";
-import type Comment from "../CommentType";
-import { computed } from "vue";
-import CommentType from "../CommentType";
+import ApiResponse from '@/core/common/types/ApiResponse'
+import { AxiosResponse } from 'axios'
+import { useGetComment } from './useGetComment'
+import { useAddComment } from './useAddComment'
+import { useDeleteComment } from './useDeleteComment'
+import { useUpdateComment } from './useUpdateComment'
+import type Comment from '../CommentType'
+import { computed, ref } from 'vue'
+import CommentType from '../CommentType'
 
 export const useCommentManage = (
-  challengeId: string,
-  commentId: string,
-  comment: CommentType,
+  challengeId: number,
   getCommentsFetch: (challengeId: string) => Promise<AxiosResponse<CommentType[] | ApiResponse>>,
-  addCommentFetch: (challengeId: string, comment: CommentType) => Promise<AxiosResponse<ApiResponse>>,
-  deleteCommentFetch: (challengeId: string, commentId: string) => Promise<AxiosResponse<ApiResponse>>,
-  updateCommentFetch: (challengeId: string, comment: CommentType) => Promise<AxiosResponse<ApiResponse>>,
-  getCommentCountFetch: (challengeId: string) => Promise<AxiosResponse<ApiResponse>>,
+  addCommentFetch: (
+    challengeId: string,
+    comment: CommentType,
+  ) => Promise<AxiosResponse<ApiResponse>>,
+  deleteCommentFetch: (
+    challengeId: string,
+    commentId: string,
+  ) => Promise<AxiosResponse<ApiResponse>>,
+  updateCommentFetch: (
+    challengeId: string,
+    comment: CommentType,
+  ) => Promise<AxiosResponse<ApiResponse>>,
 ) => {
-  const {
-    getLoading,
-    getError,
-    comments,
-    commentCnt,
-    fetchComments,
-    fetchCommentCount
-  } = useGetComment(challengeId, getCommentsFetch, getCommentCountFetch)
+  const comment = ref<CommentType>({
+    commentId: '',
+    challengeId: '',
+    commentContent: '',
+    commentAuthorId: '',
+    commentCreateDate: '',
+    commentAuthorName: '',
+  })
+  const commentId = ref('')
 
-  const {
-    createComment,
-    addLoading,
-    addError
-  } = useAddComment(challengeId, comment, addCommentFetch)
+  const { getLoading, getError, comments, commentCnt, fetchComments, fetchCommentCount } =
+    useGetComment(challengeId, getCommentsFetch)
 
-  const {
-    deleteLoading,
-    deleteError,
-    deleteComment
-  } = useDeleteComment(challengeId, commentId, deleteCommentFetch)
+  const { createComment, addLoading, addError } = useAddComment(
+    challengeId,
+    comment.value,
+    addCommentFetch,
+  )
 
-  const {
-    updateLoading,
-    updateError,
-    updateComment
-  } = useUpdateComment(challengeId, comment, updateCommentFetch)
+  const { deleteLoading, deleteError, deleteComment } = useDeleteComment(
+    challengeId,
+    commentId.value,
+    deleteCommentFetch,
+  )
 
-  const loading = computed(() => getLoading.value || addLoading.value || deleteLoading.value || updateLoading.value)
-  const error = computed(() => getError.value || addError.value || deleteError.value || updateError.value)
+  const { updateLoading, updateError, updateComment } = useUpdateComment(
+    challengeId,
+    comment.value,
+    updateCommentFetch,
+  )
+
+  const loading = computed(
+    () => getLoading.value || addLoading.value || deleteLoading.value || updateLoading.value,
+  )
+  const error = computed(
+    () => getError.value || addError.value || deleteError.value || updateError.value,
+  )
 
   const commentAdd = (comment: Comment, challengeId: string) => {
-    createComment(comment);
-    fetchComments(challengeId);
+    createComment(comment)
+    fetchComments(challengeId)
   }
 
   const commentDelete = (challengeId: string, commentId: string) => {
-    deleteComment(challengeId,commentId);
-    fetchComments(challengeId);
+    deleteComment(challengeId, commentId)
+    fetchComments(challengeId)
   }
 
   const commentUpdate = (challengeId: string, comment: CommentType) => {
-    updateComment(challengeId, comment);
-    fetchComments(challengeId);
+    updateComment(challengeId, comment)
+    fetchComments(challengeId)
   }
 
   return {
     loading,
     error,
+    comment,
     comments,
     commentCnt,
     fetchComments,
     fetchCommentCount,
     commentAdd,
     commentDelete,
-    commentUpdate
+    commentUpdate,
   }
 }
